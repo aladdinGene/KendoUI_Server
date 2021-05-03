c828c7d696f75f8f621c785ae38caa5a3fa5b29c
NikitaRaik <79733831+NikitaRaik@users.noreply.github.com>
2021-05-03 19:42:14 +0300

    redirect url

--- kendo_ui/assets/js/custom.js
+++ kendo_ui/assets/js/custom.js
@@ -9,7 +9,7 @@ var dataSourceMasterType = [
 var EMRSconfig={
     clientId: "cfc9f18c-9a43-4d6a-a556-f338be15619d",
     authority: "https://login.microsoftonline.com/171d96c1-7170-4561-a662-66c07e043e23",
-    redirectUri: "https://localhost:44354",
+    redirectUri: "https://kendoui.azurewebsites.net/",
     scopes: ["api://7b78a6e1-50a5-475d-b109-d7c18b63f513/EMRS_API"]
 };
 var loginRequest = {
