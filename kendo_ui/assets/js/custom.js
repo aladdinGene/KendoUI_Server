52588cd7f9d29a063eb265429282b82c8dccc65d
NikitaRaik <79733831+NikitaRaik@users.noreply.github.com>
2021-05-13 02:03:18 -0700

    redirect fix

--- kendo_ui/assets/js/custom.js
+++ kendo_ui/assets/js/custom.js
@@ -17,8 +17,8 @@ var loader = $('#loader').kendoLoader({
 var EMRSconfig={
     clientId: "cfc9f18c-9a43-4d6a-a556-f338be15619d",
     authority: "https://login.microsoftonline.com/171d96c1-7170-4561-a662-66c07e043e23",
-    // redirectUri: "https://kendoui.azurewebsites.net",
-    redirectUri: "http://localhost:44354",
+    redirectUri: "https://kendoui.azurewebsites.net",
+    // redirectUri: "http://localhost:44354",
     scopes: ["api://7b78a6e1-50a5-475d-b109-d7c18b63f513/EMRS_API"]
 };
 var loginRequest = {
@@ -472,7 +472,7 @@ let reference_pop = $("#reference-pop").kendoWindow({
     content: {
         iframe: true
     },
-    actions: ["Minimize", "Maximize", "Close"],
+    actions: ["Minimize", "Close"],
     draggable: true,
     resizable: false,
     width: "500px",
@@ -825,7 +825,7 @@ $(document).ready(function() {
         content: {
             iframe: true
         },
-        actions: ["Custom", "Minimize", "Maximize", "Close"],
+        actions: ["Custom", "Minimize", "Close"],
         draggable: true,
         resizable: false,
         width: "500px",
@@ -1091,7 +1091,7 @@ $(document).ready(function() {
         content: {
             iframe: true
         },
-        actions: ["Minimize", "Maximize", "Close"],
+        actions: ["Minimize", "Close"],
         draggable: true,
         resizable: false,
         width: "500px",
